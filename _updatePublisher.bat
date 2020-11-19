0d307b160982878366b6ff3e82a77a390dd27b74
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2020-11-19 07:41:46 -0800

    Correcting the version number of the IG in the title Correcting the URL of where the FHIR Publisher is downloaded from in the batch file

--- _updatePublisher.bat
+++ _updatePublisher.bat
@@ -1,6 +1,6 @@
   
 @MKDIR input-cache
 @ECHO "Downloading most recent publisher - it's ~100 MB, so this may take a bit
-@POWERSHELL -command (new-object System.Net.WebClient).DownloadFile(\"https://storage.googleapis.com/ig-build/org.hl7.fhir.publisher.jar\",\"org.hl7.fhir.publisher.jar\")
+@POWERSHELL -command (new-object System.Net.WebClient).DownloadFile(\"https://github.com/HL7/fhir-ig-publisher/releases/latest/download/publisher.jar\",\"org.hl7.fhir.publisher.jar\")
 @ECHO "Done"
 @PAUSE
